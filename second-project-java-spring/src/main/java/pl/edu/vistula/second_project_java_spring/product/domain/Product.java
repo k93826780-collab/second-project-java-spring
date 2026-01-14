f0439e0fcabe88beafa5bdffce2ee1b48a8d8832
Kyrylo Voloshchenko <k93826780@gmail.com>
2026-01-14 02:07:43 +0100

    update

--- second-project-java-spring/src/main/java/pl/edu/vistula/second_project_java_spring/product/domain/Product.java
+++ second-project-java-spring/src/main/java/pl/edu/vistula/second_project_java_spring/product/domain/Product.java
@@ -1,11 +1,18 @@
 package pl.edu.vistula.second_project_java_spring.product.domain;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "product")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
 }
